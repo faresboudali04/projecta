a9e3f480d2231a9a7452a5c4c4dc0bb13ebd0204
Your Name <your.email@example.com>
2025-09-30 16:13:52 +0200

    Add working train.py, requirements, and .gitignore

---
+++ train.py
@@ -0,0 +1,11 @@
+﻿import pandas as pd
+from sklearn.linear_model import LinearRegression
+import joblib
+
+data = pd.DataFrame({"x":[1,2,3,4,5],"y":[2,4,6,8,10]})
+X = data[["x"]]
+y = data["y"]
+
+model = LinearRegression().fit(X, y)
+joblib.dump(model, "model.pkl")
+print("Model trained and saved!")
